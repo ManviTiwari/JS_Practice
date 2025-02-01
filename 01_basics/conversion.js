3dd0e03fa5ccfde14633cf64260e7e88a499c366
ManviTiwari <89415015+ManviTiwari@users.noreply.github.com>
2025-02-01 10:04:17 +0000

    basics practice

---
+++ 01_basics/conversion.js
@@ -0,0 +1,38 @@
+let score = undefined;
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // NaN
+
+// Score => (convert into Number)
+//     23 => 23
+//     "23" => 23
+//     "23aa" => NaN
+//     null => 0
+//     undifned => NaN
+//      true => 1
+
+let isLoggedIn = undefined;
+
+let booleanInValue = Boolean(isLoggedIn);
+
+//console.log(booleanInValue);
+
+// Convert value into Boolean
+//     1 => true
+//     "Manvi" => true
+//     null => false
+//     undefined => false
+ //     "" => false;
+
+ let val = undefined;
+ 
+ valueInString = String(val);
+
+ console.log(typeof valueInString)
+ console.log(valueInString)
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+const accountNumber = 1;
+
+let accountName = "Saving";
+
+var accountCity = "Kanpur";
+
+// accountNumber = 2;  Can't change constant value
+
+accountName = "Current";
+
+console.log(accountName);
+
+
